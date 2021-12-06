464f3214f6d12ef6f3f5e3ae3f88d8016d2e5635
Linus Ericsson <linus.ericsson@ed.ac.uk>
2021-12-06 17:43:42 +0000

    Update eval_causal3dident.py

--- eval_causal3dident.py
+++ eval_causal3dident.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, models, transforms
-from torch.utils.data import DataLoader, ConcatDataset, Subset
+from torch.utils.data import DataLoader
 
 import os
 import inspect
